8aa07db5748146c0531f388e8bc5c7f992242407
surrsurus <graymury@gmail.com>
2017-10-06 08:41:15 -0400

    Integrate ROT.js as in #4

---
+++ game/onload.js
@@ -0,0 +1,11 @@
+// Load up the game
+window.onload = function() {
+    
+  // Check if rot.js can work on this browser
+  if (!ROT.isSupported()) {
+    
+    alert("The rot.js library isn't supported by your browser.");
+
+  }
+
+}
