0df74ec2518e79fec18805d3067f4ee1cd0de3e5
surrsurus <graymury@gmail.com>
2017-10-06 23:24:00 -0400

    Add screen system

--- game/game.js
+++ game/game.js
@@ -1,6 +1,6 @@
 /**
- * This is the file that contains the Game object that controls all entities the game needs to function
- * The game will automatically call its main function as soon as this file is added via a script tag in an html file
+ * This is the file that contains all Screen objects used by the game to handle game flow as well as a
+ * self executing main function that runs as soon as the file is included in an html file
  */
 
  // Game objects
@@ -17,18 +17,57 @@ const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
 /**
- * @desc Game class handles rendering everything and updating all elements each tick, 
- * though does not do this in a loop on its own
- * @class
+ * @desc Superclass for screens
+ * 
+ * Idea behind screens
+ * 
+ * By having a set "current screen" (and assuming a screen can be updated and rendered) we can choose what to render
+ * and update, and allow for switching between a pause, title, and game screen for examples easily
+ * 
+ * @abstract
  */
-class Game {
+class Screen {
 
   /**
-   * @desc Initialize the canvas, player positon, and camera.
-   * Assumes canvas is already initialized
+   * @desc Constructor for superclass
+   * 
+   * We make the assumption that any necessary canvas initialization or management happens elsewhere, for example
+   * onload or at the start of a main function
+   * 
+   * @constructor
+   */
+  constructor() {}
+
+  /**
+   * @desc Render objects held by the screen
+   * @method
+   */
+  render() { throw Exception('Screen is an abstract class, override this method'); }
+
+  /**
+   * @desc Update objects held by the screen every tick
+   * @method
+   * 
+   * @param {Number} dt Datetime
+   */
+  update(dt) { throw Exception('Screen is an abstract class, override this method'); }
+
+}
+
+/**
+ * @desc Game Screen handles the updating and rendering of game objects
+ * @class
+ */
+class GameScreen extends Screen {
+
+  /**
+   * @desc Initialize the player positon, and camera.
    * @constructor
    */
   constructor() {
+
+    // Call Screen constructor
+    super();
 
     // Place the player
     [player.x, player.y] = environment.findStart();
@@ -40,8 +79,9 @@ class Game {
   }
 
   /**
-   * @desc Render all the objects to the canvas
-   * @function
+   * @desc Render all the game objects to the canvas
+   * namely the environment and player
+   * @method
    */
   render() {
     
@@ -56,8 +96,8 @@ class Game {
   }
 
   /**
-   * @desc Update every object
-   * @function
+   * @desc Update every object, namely the environment and player
+   * @method
    * 
    * @param {Number} dt Datetime
    */
@@ -80,31 +120,80 @@ class Game {
 
 }
 
-class TitleScreen {
+/**
+ * @desc Display a static image as the tile screen and go to the game screen on a button press
+ * @class
+ */
+class TitleScreen extends Screen {
+
+  /**
+   * @desc Initialize the title screen image
+   * @constructor
+   */
   constructor() {
+
+    // Call Screen constructor
+    super();
+
+    // Create an image object
     this.title = new Image();
     this.title.src = "./assets/ui/title.png";
+
   }
 
+  /**
+   * @desc Render the title screen image
+   * @method
+   */
   render() {
     ctx.drawImage(this.title, 0, 0);
   }
 
-  update() {
+  /**
+   * @desc Check for event to transition to game screen
+   * @method
+   * 
+   * @param {Number} dt Datetime
+   */
+  update(dt) {
+
+    // Check for specific event
     if (global.startGame) {
+
+      // Clear screen
       ctx.fillStyle = 'black';
       ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
       ctx.save();
-      global.currScreen = new Game();
+
+      // Valid since declaration happens before this gets a chance to run
+      // If you have errors relating to TitleScreen.update(), this may be it.
+
+      // Transfer ownership to a game screen
+      currentScreen = gameScreen;
+
     }
+
   }
 
 }
 
- /**
-   * @desc Main function that makes the game object render and update animations in a loop
-   * @main
-   */
+/**
+ * Establish the current screen object for this file.
+ * Should be accessible by each Screen subclass since we should only be using them after
+ * the declaration of this variable
+ */
+let currentScreen = new TitleScreen();
+
+/**
+ * Instantiate a game screen object to generate an environment so switching ownership to it
+ * is faster
+ */
+let gameScreen = new GameScreen();
+
+/**
+  * @desc Main function that auto-executes and makes the game object render and update animations in a loop
+  * @main
+  */
 const main = (function(){
 
   /**
@@ -123,7 +212,7 @@ const main = (function(){
   let lastTime;
 
   // Title screen
-  global.currScreen = new TitleScreen();
+  currentScreen = new TitleScreen();
 
   /**
    * @desc This function represents a tick with respect to the game. 
@@ -136,8 +225,8 @@ const main = (function(){
     let now = Date.now();
     let dt = (now - lastTime) / 1000.0;
 
-    global.currScreen.update(dt);
-    global.currScreen.render();
+    currentScreen.update(dt);
+    currentScreen.render();
 
     lastTime = now;
 
