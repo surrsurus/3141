8c2068a11f0e80a250ef337e8b5c4043427bd160
surrsurus <graymury@gmail.com>
2017-10-07 21:34:39 -0400

    Alphabetize and encapsulate environment

--- game/environment.js
+++ game/environment.js
@@ -32,173 +32,27 @@ class Environment {
   }
 
   /**
-   * @desc Generate a dungeon using the dungeon generator 
-   * This also sets the bounds for the dungeon
-   * 
-   * @method
+   * PRIVATE
    */
-  genDungeon() {
-
-    // Generate a dungeon
-    this.dungeon = new Dungeon().generate({
-      width: S.mapWidth,
-      height: S.mapHeight
-    });
-  
-    this.setBounds();
-
-  }
 
   /**
-   * @desc Clear existing boundaries and set them for each tile in the dungeon map
-   * WARNING: Enterprise quality code ahead
-   * 
-   * @method
-   */
-  setBounds() {
-
-    // Clear bounds
-    this.bounds = [];
-    
-    // Purposefully trigger exceptions by accessing oob elements
-    for (let x = -1; x < this.dungeon.tiles.length + 1; x++) {
-
-      for (let y = -1; y < this.dungeon.tiles.length + 1; y++) {
-
-        try {
-
-          // Walls recieve boundaries
-          if (this.dungeon.tiles[x][y].type === 'wall') {
-            this.pushBounds(x, y);
-          }
-
-        }
-
-        // Since index errors are bound to happen, we know that if an index error occurs,
-        // the tile we're looking at doesn't exist on the map, therefore we can draw a boundary around it
-        // to prevent player from leaving the map if a floor tile spawns on the edge
-        catch (e) {
-          this.pushBounds(x, y);
-        }
-
-      }
-
-    }
-
-  }
-
-  /**
-   * @desc Determines if two rectangles are overlapping
+   * @desc Outline the boundary boxes of an object
    * @method
    * 
-   * @param {Object} r1 - First rectangle to compare
-   * @param {Object} r2 - Second rectangle to compare
-   * @return {Boolean} Returns true if overlapping, false if not
+   * @param {Number} x - x coord of object 
+   * @param {*} y - y coord of object
+   * @param {*} width - Width of object
+   * @param {*} height - Height of object
+   * @param {*} ctx - Canvas context
    */
-  intersectRect(r1, r2) {
-    return (r2.left < r1.right ||
-             r2.right > r1.left ||
-             r2.top < r1.bottom ||
-             r2.bottom > r1.top);
-  }
-
-  /**
-   * @desc Add a set of cartesian boundaries to the boundary list after converting them to isometric
-   * @method
-   * 
-   * @param {Number} x - x coord
-   * @param {Number} y - y coord 
-   */
-  pushBounds(x, y) {
-
-    let cartX = x * S.tileWidth / 2;
-    let cartY = y * S.tileHeight;
-    let isoX = cartX - cartY;
-    let isoY = (cartX + cartY) / 2;
-
-    this.bounds.push({
-      top: isoY,
-      left: isoX,
-      right: isoX + S.tileWidth,
-      bottom: isoY + S.tileHeight
-    });
-
-  }
-
-  /**
-   * @desc Find the start of the map, usually the top-left most tile 
-   * Good for setting the player location
-   * TODO: Probably just want to make a function that gets a random tile instead
-   * @method
-   * 
-   * @returns {Array} Array of isometric coords that represent the tile where the map starts
-   */
-  findStart() {
-    let startX;
-    let startY;
-    let done = false;
-    for (let x = 0; x < this.dungeon.tiles.length; x++) {
-      if (done) {
-        break;
-      }
-      for (let y = 0; y < this.dungeon.tiles.length; y++) {
-        if (this.dungeon.tiles[x][y].type !== 'wall') {
-          console.log(x, y);
-          // startX = x * S.tileWidth / 2 + S.tileWidth / 2;
-          // startY = y * S.tileHeight + S.tileHeight / 2;
-          startX = x * S.tileWidth / 2;
-          startY = y * S.tileHeight;
-          done = true;
-          break;
-        }
-      }
-    }
-  
-    return U.cart2Iso(startX, startY);
-  }
-
-  /**
-   * @desc Update the environment and it's held objects
-   * Clearly does nothing, but thats because the environment has nothing to update as of right now
-   * @method
-   */
-  update() {}
-
-  /**
-   * @desc Render the map to the screen 
-   * @param {*} ctx 
-   * @param {*} camera 
-   */
-  render(ctx, camera) {
-  
-    ctx.translate(camera.offsetX, camera.offsetY);
-  
-    for (let x = 0; x < this.dungeon.tiles.length; x++) {
-      for (let y = 0; y < this.dungeon.tiles.length; y++) {
-
-        // Draw floor tiles as white tiles
-        if (this.dungeon.tiles[x][y].type === 'floor')
-          this.drawTile(x, y, ctx);
-
-        // Draw door tiles as red tiles
-        else if (this.dungeon.tiles[x][y].type === 'door')
-          this.drawTile(x, y, ctx, '#ffaaaa', '#ff0000');
-
-      }
-    }
-  
-    // Draw the boundaries if debug is on
-    if (global.debug) {
-
-      ctx.strokeStyle = 'red';
-
-      this.bounds.forEach((box) => {
-        this.outlineBounds(box.left, box.top, box.right - box.left, box.bottom - box.top, ctx);
-      });
-
-    }
-  
-    ctx.restore();
+  __drawBounds(x, y, width, height, ctx) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + width / 2, y + height / 2);
+    ctx.lineTo(x, y + height);
+    ctx.lineTo(x - width / 2, y + height / 2);
+    ctx.lineTo(x, y);
+    ctx.stroke();
   }
 
   /**
@@ -211,7 +65,7 @@ class Environment {
    * @param {String} top - Top color
    * @param {String} side - Side color
    */
-  drawTile(x, y, ctx, top = '#eeeeee', side = '#dddddd') {
+  __drawTile(x, y, ctx, top = '#eeeeee', side = '#dddddd') {
 
     let cartX = x * S.tileWidth / 2;
     let cartY = y * S.tileHeight;
@@ -261,60 +115,6 @@ class Environment {
     ctx.lineTo(isoX, isoY);
     ctx.fill();
   }
-  
-  /**
-   * @desc Outline the boundary boxes of an object
-   * @method
-   * 
-   * @param {Number} x - x coord of object 
-   * @param {*} y - y coord of object
-   * @param {*} width - Width of object
-   * @param {*} height - Height of object
-   * @param {*} ctx - Canvas context
-   */
-  outlineBounds(x, y, width, height, ctx) {
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(x + width / 2, y + height / 2);
-    ctx.lineTo(x, y + height);
-    ctx.lineTo(x - width / 2, y + height / 2);
-    ctx.lineTo(x, y);
-    ctx.stroke();
-  }
-
-  /**
-   * @desc Render all objects in the foreground
-   * @method
-   * 
-   * @param {Object} ctx - Canvas context
-   * @param {Object} camera - Camera object
-   */
-  renderForeground(ctx, camera) {
-    ctx.save();
-    ctx.translate(camera.offsetX, camera.offsetY);
-    // ctx.drawImage(this.foregroundImg, 0, 0);
-    ctx.restore();
-  }
-
-  /**
-   * @desc Compare boundary boxes to determine if an entity moves out of bounds
-   * @method
-   * 
-   * @param {Object} boundingBox - Object of entity that can go out of bounds
-   * @return {Boolean} True if the object is out of bounds, false if it is not
-   */
-  isOutOfBounds(boundingBox) {
-
-    // Basically just see if the bounding box overlaps any others
-    for (let bound in this.bounds) {
-      if (this.intersectIsometric(boundingBox, this.bounds[bound])) {
-        return true;
-      }
-    }
-  
-    return false;
-
-  }
 
   /**
    * @desc Determine if two isometric rectangles are intersecting
@@ -324,9 +124,9 @@ class Environment {
    * @param {Object} r2 - Iso rectangle 2
    * @return {Boolean} True if they are overlapping, false if not
    */
-  intersectIsometric(r1, r2) {
+  __intersectIsometric(r1, r2) {
     // Check outer bounding box first
-    if (!this.intersectRect(r1, r2)) {
+    if (!this.__intersectRect(r1, r2)) {
       return false;
     }
   
@@ -356,7 +156,217 @@ class Environment {
     ]]);
   
     return overlaps(poly1, poly2);
+
   }
+
+  /**
+   * @desc Determines if two rectangles are overlapping
+   * @method
+   * 
+   * @param {Object} r1 - First rectangle to compare
+   * @param {Object} r2 - Second rectangle to compare
+   * @return {Boolean} Returns true if overlapping, false if not
+   */
+  __intersectRect(r1, r2) {
+    return (r2.left < r1.right ||
+             r2.right > r1.left ||
+             r2.top < r1.bottom ||
+             r2.bottom > r1.top);
+  }
+
+  /**
+   * @desc Add a set of cartesian boundaries to the boundary list after converting them to isometric
+   * @method
+   * 
+   * @param {Number} x - x coord
+   * @param {Number} y - y coord 
+   */
+  __pushBounds(x, y) {
+
+    let cartX = x * S.tileWidth / 2;
+    let cartY = y * S.tileHeight;
+    let isoX = cartX - cartY;
+    let isoY = (cartX + cartY) / 2;
+
+    this.bounds.push({
+      top: isoY,
+      left: isoX,
+      right: isoX + S.tileWidth,
+      bottom: isoY + S.tileHeight
+    });
+
+  }
+
+  /**
+   * @desc Clear existing boundaries and set them for each tile in the dungeon map
+   * WARNING: Enterprise quality code ahead
+   * 
+   * @method
+   */
+  __setBounds() {
+
+    // Clear bounds
+    this.bounds = [];
+    
+    // Purposefully trigger exceptions by accessing oob elements
+    for (let x = -1; x < this.dungeon.tiles.length + 1; x++) {
+
+      for (let y = -1; y < this.dungeon.tiles.length + 1; y++) {
+
+        try {
+
+          // Walls recieve boundaries
+          if (this.dungeon.tiles[x][y].type === 'wall') {
+            this.__pushBounds(x, y);
+          }
+
+        }
+
+        // Since index errors are bound to happen, we know that if an index error occurs,
+        // the tile we're looking at doesn't exist on the map, therefore we can draw a boundary around it
+        // to prevent player from leaving the map if a floor tile spawns on the edge
+        catch (e) {
+          this.__pushBounds(x, y);
+        }
+
+      }
+
+    }
+
+  }
+
+  /**
+   * PUBLIC
+   */
+
+  /**
+   * @desc Find the start of the map, usually the top-left most tile 
+   * Good for setting the player location
+   * TODO: Probably just want to make a function that gets a random tile instead
+   * @method
+   * 
+   * @returns {Array} Array of isometric coords that represent the tile where the map starts
+   */
+  findStart() {
+    let startX;
+    let startY;
+
+    for (let x = 0; x < this.dungeon.tiles.length; x++) {
+      for (let y = 0; y < this.dungeon.tiles.length; y++) {
+
+        if (this.dungeon.tiles[x][y].type !== 'wall') {
+
+          console.log(x, y);
+          // startX = x * S.tileWidth / 2 + S.tileWidth / 2;
+          // startY = y * S.tileHeight + S.tileHeight / 2;
+          startX = x * S.tileWidth / 2;
+          startY = y * S.tileHeight;
+
+          return U.cart2Iso(startX, startY);
+
+        }
+      }
+    }
+  
+  }
+
+  /**
+   * @desc Generate a dungeon using the dungeon generator 
+   * This also sets the bounds for the dungeon
+   * 
+   * @method
+   */
+  genDungeon() {
+
+    // Generate a dungeon
+    this.dungeon = new Dungeon().generate({
+      width: S.mapWidth,
+      height: S.mapHeight
+    });
+  
+    this.__setBounds();
+
+  }
+
+    /**
+   * @desc Compare boundary boxes to determine if an entity moves out of bounds
+   * @method
+   * 
+   * @param {Object} boundingBox - Object of entity that can go out of bounds
+   * @return {Boolean} True if the object is out of bounds, false if it is not
+   */
+  isOutOfBounds(boundingBox) {
+
+    // Basically just see if the bounding box overlaps any others
+    for (let bound in this.bounds) {
+      if (this.__intersectIsometric(boundingBox, this.bounds[bound])) {
+        return true;
+      }
+    }
+  
+    return false;
+
+  }
+
+  /**
+   * @desc Render the map to the screen 
+   * @param {*} ctx 
+   * @param {*} camera 
+   */
+  render(ctx, camera) {
+  
+    ctx.translate(camera.offsetX, camera.offsetY);
+  
+    for (let x = 0; x < this.dungeon.tiles.length; x++) {
+      for (let y = 0; y < this.dungeon.tiles.length; y++) {
+
+        // Draw floor tiles as white tiles
+        if (this.dungeon.tiles[x][y].type === 'floor')
+          this.__drawTile(x, y, ctx);
+
+        // Draw door tiles as red tiles
+        else if (this.dungeon.tiles[x][y].type === 'door')
+          this.__drawTile(x, y, ctx, '#ffaaaa', '#ff0000');
+
+      }
+    }
+  
+    // Draw the boundaries if debug is on
+    if (global.debug) {
+
+      ctx.strokeStyle = 'red';
+
+      this.bounds.forEach((box) => {
+        this.__drawBounds(box.left, box.top, box.right - box.left, box.bottom - box.top, ctx);
+      });
+
+    }
+  
+    ctx.restore();
+
+  }
+
+  /**
+   * @desc Render all objects in the foreground
+   * WARN: Not used
+   * @method
+   * 
+   * @param {Object} ctx - Canvas context
+   * @param {Object} camera - Camera object
+   */
+  renderForeground(ctx, camera) {
+    ctx.save();
+    ctx.translate(camera.offsetX, camera.offsetY);
+    // ctx.drawImage(this.foregroundImg, 0, 0);
+    ctx.restore();
+  }
+  
+  /**
+   * @desc Update the environment and it's held objects
+   * Clearly does nothing, but thats because the environment has nothing to update as of right now
+   * @method
+   */
+  update() {}
   
 }
 
